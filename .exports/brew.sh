da815e2c711c34041eab36f85bb95417bf13b73e
Derek Chafin <infomaniac50@gmail.com>
2017-04-11 19:38:18 -0500

    Don't choke if we can't find brew

--- .exports/brew.sh
+++ .exports/brew.sh
@@ -1,12 +1,25 @@
 # vim:set filetype=sh:
 
-brew_prefix=$(brew --prefix)
+if which brew > /dev/null; then
+    brew_prefix=$(brew --prefix)
+    export HOMEBREW_FOUND=1
+elif [ -e $HOME/.linuxbrew/bin/brew ]; then
+    brew_prefix=$($HOME/.linuxbrew/bin/brew --prefix)
+    export HOMEBREW_FOUND=1
+elif [ -e /usr/local/bin/brew ]; then
+    brew_prefix=$(/usr/local/bin/brew --prefix)
+    export HOMEBREW_FOUND=1
+else
+    export HOMEBREW_FOUND=0
+fi
 
-pathprepend $brew_prefix/bin PATH
-pathprepend $brew_prefix/share/man MANPATH
-pathprepend $brew_prefix/share/info INFOPATH
-pathprepend $brew_prefix/share XDG_DATA_DIRS
+if [[ $HOMEBREW_FOUND -eq 1 ]]; then
+    pathprepend $brew_prefix/bin PATH
+    pathprepend $brew_prefix/share/man MANPATH
+    pathprepend $brew_prefix/share/info INFOPATH
+    pathprepend $brew_prefix/share XDG_DATA_DIRS
 
-ssource "$brew_prefix/etc/bash_completion"
+    ssource "$brew_prefix/etc/bash_completion"
 
-unset brew_prefix
+    unset brew_prefix
+fi
