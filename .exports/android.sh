bfabc7d9b07c2baa70199a2afc1ef9a2b02d1f1e
Derek Chafin <infomaniac50@gmail.com>
2017-04-28 17:02:54 -0500

    Added convenience function to run Android emulators by name.

--- .exports/android.sh
+++ .exports/android.sh
@@ -1,3 +1,10 @@
 export ANDROID_HOME=${HOME}/prefix/opt/android-sdk
 pathappend $ANDROID_HOME/tools
 pathappend $ANDROID_HOME/platform-tools
+
+# You won't get any error messages with this. So if your emulator doesn't seem to start, then try running it directly.
+# Let's you run your emulators like <avd name> <emulator options>.
+# See emulator -help for emulator options reference.
+for avd in $(emulator -list-avds); do
+    eval "$avd() { nohup emulator @$avd \$@ > /dev/null 2> /dev/null & disown; }";
+done
