f87b621e42f2f27ee42e1506b105e389e694b95a
Derek Chafin <infomaniac50@gmail.com>
2017-05-03 21:42:40 -0500

    Remove unused workflow function.

--- .exports/workflow.sh
+++ .exports/workflow.sh
@@ -20,42 +20,6 @@ askyesno()
     fi
 }
 
-try_source_path()
-{
-    RELPATH="$1"
-    EXTENSION="$2"
-
-    SYS_MODULE_PATH="$BASHWF_SYS/${RELPATH}"
-
-    # Load our bashrc modules if they exist
-    if [ -d "$SYS_MODULE_PATH" ]; then
-
-        for MODULE in $SYS_MODULE_PATH/*.$EXTENSION
-        do
-            MODULE_NAME=${MODULE##*/}
-            MODULE_NAME=${MODULE_NAME%.*}
-            printf "%s: " $MODULE_NAME
-            . $MODULE
-        done
-
-    fi
-
-    USER_MODULE_PATH="$BASHWF_USER/${RELPATH}"
-
-    # Load our bashrc modules if they exist
-    if [ -d "$USER_MODULE_PATH" ]; then
-
-        for MODULE in $USER_MODULE_PATH/*.$EXTENSION
-        do
-            MODULE_NAME=${MODULE##*/}
-            MODULE_NAME=${MODULE_NAME%.*}
-            printf "%s: " $MODULE_NAME
-            . $MODULE
-        done
-
-    fi
-}
-
 # searching functions
 find_in_ancestor()
 {
