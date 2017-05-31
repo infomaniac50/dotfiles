8e90b3248bba7e89c5640a188983981a37dc8de7
Derek Chafin <infomaniac50@gmail.com>
2017-05-30 23:26:52 -0500

    Added Android Studio to PATH.

--- .exports/android.sh
+++ .exports/android.sh
@@ -1,6 +1,12 @@
 export ANDROID_HOME=${HOME}/prefix/opt/android-sdk
+export ANDROID_STUDIO=${HOME}/prefix/opt/android-studio
+
 pathappend $ANDROID_HOME/tools
+pathappend $ANDROID_HOME/tools/bin
 pathappend $ANDROID_HOME/platform-tools
+
+pathappend $ANDROID_STUDIO/gradle/gradle-3.2/bin
+pathappend $ANDROID_STUDIO/bin
 
 if command_exists emulator; then
   # You won't get any error messages with this. So if your emulator doesn't seem to start, then try running it directly.
