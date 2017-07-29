f5a71fbb9ea8e70a0e49c060747b24dbcd75f391
Derek Chafin <infomaniac50@gmail.com>
2017-07-29 05:24:51 -0500

    Move defaults to .exports/defaults.sh so they can be re-loaded too.

--- .exports.sh
+++ .exports.sh
@@ -3,6 +3,4 @@
 ssource ${HOME}/.exports/eload.sh
 ssource ${HOME}/.exports/loader.sh
 ssource ${HOME}/.exports/secrets.sh
-
-alias loaderdefault='loader liquidprompt brew prefix fasd cabel golang pypi node composer phpbrew android trash rvm workflow'
-alias dload='loaderdefault'
+ssource ${HOME}/.exports/default.sh
