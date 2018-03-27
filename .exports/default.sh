57e4ff3e520f3883fffead02cdead67c8190fa06
Derek Chafin <infomaniac50@gmail.com>
2018-03-27 02:18:25 -0500

    Load nvm by default.

--- .exports/default.sh
+++ .exports/default.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # vim:set filetype=sh:
 
-alias loaderdefault='loader liquidprompt brew prefix fasd cabel golang pypi node composer phpbrew android trash rvm workflow bin'
+alias loaderdefault='loader liquidprompt brew prefix fasd cabel golang pypi node nvm composer phpbrew android trash rvm workflow bin'
 alias dload='loaderdefault'
