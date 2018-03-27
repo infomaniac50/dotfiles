a38ace2f76ff49ceab33b6494460ddf903634d7e
Derek Chafin <infomaniac50@gmail.com>
2018-03-27 02:33:32 -0500

    We don't need PATH variables for NODE with nvm installed.

--- .exports/node.sh
+++ .exports/node.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # vim:set filetype=sh:
 
-export NPM_PACKAGES="$HOME/.npm-packages"
-
 # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
 # unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
 # export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-
-pathprepend $NPM_PACKAGES/lib/node_modules NODE_PATH
-pathprepend $NPM_PACKAGES/bin PATH
-pathprepend node_modules/.bin
