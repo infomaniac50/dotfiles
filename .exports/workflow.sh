4b937d34e2ce849ddf36e18b3fd55c666edb18b7
Derek Chafin <infomaniac50@gmail.com>
2018-08-10 23:08:26 -0500

    Quote this to prevent word splitting.

--- .exports/workflow.sh
+++ .exports/workflow.sh
@@ -27,8 +27,8 @@ if command_exists docker; then
   alias dkip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
   alias dkd="docker run -d -P"
   alias dki="docker run -t -i -P"
-  dkrm() { docker rm $(docker ps -q -a); }
-  dkri() { docker rmi $(docker images -q); }
+  dkrm() { docker rm "$(docker ps -q -a)"; }
+  dkri() { docker rmi "$(docker images -q)"; }
   dkb() { docker build -t="$1" .; }
   # These are mine
   # Remove all untagged images
