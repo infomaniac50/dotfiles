d67b9e967aae75686abea131ad4572fc72ad0366
Derek Chafin <infomaniac50@gmail.com>
2018-08-29 15:59:26 -0500

    Added a script to easily vendor the GOPATH.

--- .exports/golang.sh
+++ .exports/golang.sh
@@ -1,6 +1,39 @@
 #!/usr/bin/env bash
 # vim:set filetype=sh:
 
-#GOROOT=$HOME/prefix/opt/go
-export GOPATH=$HOME/workspace/go
-pathprepend $GOPATH/bin PATH
+function gopath_defaults() {
+  #GOROOT=$HOME/prefix/opt/go
+  if [ ! -z $GOPATH ] && [ -d $GOPATH/bin ]; then
+    pathremove $GOPATH/bin PATH
+  fi
+  export GOPATH=$HOME/workspace/go
+  pathprepend $GOPATH/bin PATH
+}
+
+# Derived from https://hgfischer.org/article/managing-multiple-gopaths/
+function gopath() {
+  local cdir=$PWD
+  local found=0
+
+  # Look for .gopath in current and parent directories.
+  # If found then set GOPATH.
+  while [ "$cdir" != "/" ]; do
+    if [ -e "$cdir/.gopath" ]; then
+      if [ ! -z $GOPATH ] && [ -d $GOPATH/bin ]; then
+        pathremove $GOPATH/bin PATH
+      fi
+      export GOPATH=$cdir
+      pathprepend $GOPATH/bin PATH
+      found=1
+      break
+    fi
+    cdir=$(dirname "$cdir")
+  done
+
+  # If not found then set GOPATH defaults.
+  if [[ $found -eq 0 ]]; then
+    gopath_defaults
+  fi
+}
+
+gopath_defaults
