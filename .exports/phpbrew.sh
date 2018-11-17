81f663c86f48bb275da99780c2a882e6925bfe88
Derek Chafin <infomaniac50@gmail.com>
2018-11-17 01:35:01 -0600

    Experimental PHP installer shortcut using PHPBrew.

--- .exports/phpbrew.sh
+++ .exports/phpbrew.sh
@@ -4,3 +4,27 @@
 pathprepend ${HOME}/prefix/opt/phpbrew/bin PATH
 
 ssource "$HOME/.phpbrew/bashrc"
+
+function dotfiles-install-php-variants()
+{
+	if [[ -z $infomaniac50_install_php_version ]]; then
+		echo "You must set infomaniac50_install_php_version before running this script."
+		return 1
+	fi
+
+	phpbrew install --jobs=2 $infomaniac50_install_php_version `cat ${HOME}/prefix/phpbrew-variants.txt`
+}
+
+function dotfiles-install-php-extensions()
+{
+	if [[ -z $infomaniac50_install_php_version ]]; then
+		echo "You must set infomaniac50_install_php_version before running this script."
+		return 1
+	fi
+
+	phpbrew use $infomaniac50_install_php_version
+
+	for ext in $(cat ${HOME}/prefix/phpbrew-extensions.txt); do 
+		phpbrew ext install $ext
+	done
+}
