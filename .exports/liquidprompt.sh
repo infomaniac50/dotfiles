25aaf5f9db1c3f967cb4fd85811545638614487e
Derek Chafin <infomaniac50@gmail.com>
2020-06-30 01:41:21 -0500

    Use printf instead of octal escapes.

--- .exports/liquidprompt.sh
+++ .exports/liquidprompt.sh
@@ -1,7 +1,7 @@
 # vim:set filetype=sh:
 
 # This is the lambda character from the Half-Life video game series.
-LP_MARK_DEFAULT="\316\273"
+LP_MARK_DEFAULT="$(printf '\u3bb')"
 
 ssource $HOME/liquidprompt/liquidprompt
 
