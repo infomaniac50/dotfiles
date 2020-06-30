9bf369e5feacc352228a4850f561a0a70a395cb5
Derek Chafin <infomaniac50@gmail.com>
2020-06-30 01:39:57 -0500

    Fixed meta function for ZSH.

--- .exports/meta.sh
+++ .exports/meta.sh
@@ -4,6 +4,7 @@ meta()
 {
   local implemented="Only aliases, functions, and files are currently implemented."
   local file="$1"
+  local shell=$(basename $SHELL)
 
   if [[ -z "$1" ]]; then
     cat <<EOF
@@ -16,33 +17,63 @@ EOF
     return 1
   fi
 
-  case $(type -t "$file") in
-    function)
-      echo -n "function "
-      type "$file" | tail -n+2
-      ;;
-    alias)
-      alias "$file"
-      ;;
-    file)
-      file=`type -fp "$file"`
-      if [[ -L $file ]]; then
-        file=`readlink -f "$file"`
-      fi
-      mime=$(file --brief --mime-type "$file")
+  if [[ $shell == "zsh" ]]; then
+    case $(type -w "$file" | sed "s/$file\:\ //") in
+      alias)
+        echo "# Alias $file"
+        whence -c "$file"
+        ;;
+      function)
+        echo "# Function $file"
+        whence -c "$file"
+        ;;
+      command)
+        file=`whence thing`
+        if [[ -L $file ]]; then
+          file=`readlink -f "$file"`
+        fi
+        mime=$(file --brief --mime-type "$file")
 
-      if [[ -f "$file" && $mime == text* ]]; then
-        cat "$file"
-      else
-        echo "Binary File"
-        echo $mime
-      fi
-      ;;
-    *)
-      echo $implemented
-      return 1
-      ;;
-  esac
+        if [[ -f "$file" && $mime == text* ]]; then
+          cat "$file"
+        else
+          echo "Binary File"
+          echo $mime
+        fi
+        ;;
+      *)
+        echo $implemented
+        return 1
+        ;;
+    esac
+  else
+      case $(type -t "$file") in
+        function)
+          echo -n "function "
+          type "$file" | tail -n+2
+          ;;
+        alias)
+          alias "$file"
+          ;;
+        file)
+          file=`type -fp "$file"`
+          if [[ -L $file ]]; then
+            file=`readlink -f "$file"`
+          fi
+          mime=$(file --brief --mime-type "$file")
 
+          if [[ -f "$file" && $mime == text* ]]; then
+            cat "$file"
+          else
+            echo "Binary File"
+            echo $mime
+          fi
+          ;;
+        *)
+          echo $implemented
+          return 1
+          ;;
+      esac
+  fi
   return 0
 }
