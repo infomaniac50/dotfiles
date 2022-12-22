d4ab8152d1bc3f18577bd8d53b0a9dad983d56e9
Derek Chafin <infomaniac50@gmail.com>
2022-12-22 16:18:23 -0600

    feat: Source local only config for liquidprompt if it exists

--- .exports/liquidprompt.sh
+++ .exports/liquidprompt.sh
@@ -5,4 +5,8 @@ LP_MARK_DEFAULT="$(printf '\u3bb')"
 
 ssource $HOME/liquidprompt/liquidprompt
 
+if [ -r $HOME/.exports/liquidprompt.local.sh ]; then
+	ssource $HOME/.exports/liquidprompt.local.sh
+fi
+
 # vim:set filetype=sh:
