4ecc90db61f8564fed58ca8f309d853508c2d219
Derek Chafin <infomaniac50@gmail.com>
2022-12-22 15:57:39 -0600

    feat: Add prompt to continue if previous command fails

--- .exports/php_standup.sh
+++ .exports/php_standup.sh
@@ -10,15 +10,31 @@ function php_standup()
 
 	phpbrew install --jobs=2 $php_version $(cat ~/prefix/phpbrew-variants.txt)
 
+	if [[ $? && $(read -q "REPLY?The previous command returned an error. Continue? [Y/n]") ]]; then
+		return 1;
+	fi
+
 	phpbrew switch $php_version
 
+	if [[ $? && $(read -q "REPLY?The previous command returned an error. Continue? [Y/n]") ]]; then
+		return 1;
+	fi
+
 	phpbrew ext enable opcache
+
+	if [[ $? && $(read -q "REPLY?The previous command returned an error. Continue? [Y/n]") ]]; then
+		return 1;
+	fi
 
 	local php_extension
 	while read php_extension
 	do
 		if [ ! -z $php_extension ]; then
 			phpbrew ext install $php_extension
+
+			if [[ $? && $(read -q "REPLY?The previous command returned an error. Continue? [Y/n]") ]]; then
+				return 1;
+			fi
 		fi
 	done < ~/prefix/phpbrew-extensions.txt
 }
