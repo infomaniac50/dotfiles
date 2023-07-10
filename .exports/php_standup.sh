da61279145c7a280aee1d320bbf173006b9547d1
Derek Chafin <infomaniac50@gmail.com>
2023-07-10 17:08:56 -0500

    Add script to update config files for new PHP build

--- .exports/php_standup.sh
+++ .exports/php_standup.sh
@@ -47,4 +47,59 @@ function php_standup()
 			echo ""
 		fi
 	done < ~/prefix/phpbrew-extensions.txt
+
+	echo "Updating config files"
+	local PHP_CLI_INI="$(phpbrew path etc)/cli/php.ini"
+	echo "Trying $PHP_CLI_INI"
+	if [[ -e $PHP_CLI_INI ]]; then
+		echo -n "Old Settings: "
+		grep 'memory_limit' "$PHP_CLI_INI"
+		sed --in-place --regexp-extended --expression='s/memory_limit = [0-9]+[MG]/memory_limit = 4096M/' $PHP_CLI_INI
+		local sed_return=$?
+		echo -n "New Settings: "
+		grep 'memory_limit' "$PHP_CLI_INI"
+	else
+		echo "The php-cli INI file not accessible."
+	fi
+
+	local PHP_FPM_INI="$(phpbrew path etc)/fpm/php.ini"
+	echo "Trying $PHP_FPM_INI"
+	if [[ -e $PHP_FPM_INI ]]; then
+		echo -n "Old Settings: "
+		grep 'memory_limit' "$PHP_FPM_INI"
+		sed --in-place --regexp-extended --expression='s/memory_limit = [0-9]+[MG]/memory_limit = 4096M/' $PHP_FPM_INI
+		local sed_return=$?
+		echo -n "New Settings: "
+		grep 'memory_limit' "$PHP_FPM_INI"
+	else
+		echo "The php-fpm INI file not accessible."
+	fi
+
+	local DOTFILES_PREFIX_XDEBUG_INI="$HOME/prefix/xdebug.ini"
+	local PHP_GLOBAL_XDEBUG_INI="$(phpbrew path config-scan)/xdebug.ini"
+	echo -n "Trying $PHP_GLOBAL_XDEBUG_INI "
+	if [[ -e $PHP_GLOBAL_XDEBUG_INI ]]; then
+		cat $DOTFILES_PREFIX_XDEBUG_INI > $PHP_GLOBAL_XDEBUG_INI
+		echo "Done"
+	else
+		echo "Not Found"
+	fi
+
+	local PHP_CLI_XDEBUG_INI="$(phpbrew path config-scan)/cli/xdebug.ini"
+	echo -n "Trying $PHP_CLI_XDEBUG_INI "
+	if [[ -e $PHP_CLI_XDEBUG_INI ]]; then
+		cat $DOTFILES_PREFIX_XDEBUG_INI > $PHP_CLI_XDEBUG_INI
+		echo "Done"
+	else
+		echo "Not Found"
+	fi
+
+	local PHP_FPM_XDEBUG_INI="$(phpbrew path config-scan)/fpm/xdebug.ini"
+	echo -n "Trying $PHP_FPM_XDEBUG_INI "
+	if [[ -e $PHP_FPM_XDEBUG_INI ]]; then
+		cat $DOTFILES_PREFIX_XDEBUG_INI > $PHP_FPM_XDEBUG_INI
+		echo "Done"
+	else
+		echo "Not Found"
+	fi
 }
